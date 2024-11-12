0759fb813fb4ef614f8e5efa0433db07c4d00e36
siebentod <33163165+siebentod@users.noreply.github.com>
2024-11-12 20:36:03 +0300

    remove unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import './App.scss';
-import { Helmet } from 'react-helmet';
-import Footer from './Footer';
 import Input from './Input';
 import { useState } from 'react';
-import Toast from './Toast';
 
 function App() {
   const [done, setDone] = useState(false);
-  const [toastShown, setToastShown] = useState(false);
 
   return (
     <>
       <main>
-        <Input done={done} setDone={setDone} setToastShown={setToastShown} />
+        <Input done={done} setDone={setDone} />
       </main>
-      {toastShown && <Toast setToastShown={setToastShown}>Done!</Toast>}
-      <Footer />
     </>
   );
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,15 +1,19 @@
 import { useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
-function Input({ done, setDone, setToastShown }) {
+function Input({ done, setDone }) {
   const [input, setInput] = useState('');
+  const [copied, setCopied] = useState(false);
   const textareaRef = useRef(null);
+  // let numbers = [];
 
   const handleInput = (e) => {
     // console.log(e.target.value.length, ', ', input.length);
     if (Math.abs(e.target.value.length - input.length) > 1)
       setInput(processPdf(e.target.value));
     else e.target.value = input;
+    // numbers = input.split('\n').map((_, index) => index + 1);
+    // console.log(numbers);
   };
 
   const handleContainerClick = () => {
@@ -17,9 +21,9 @@ function Input({ done, setDone, setToastShown }) {
   };
 
   const processPdf = (text) => {
-    console.log(text.split(''));
+    // console.log(text.split(''));
     for (let i = 0; i < 40; i++) {
-      console.log(text[i], text.charCodeAt(i).toString(16));
+      // console.log(text[i], text.charCodeAt(i).toString(16));
     }
     let updatedText = text.replace(/\u00AD\n/g, '');
 
@@ -38,33 +42,43 @@ function Input({ done, setDone, setToastShown }) {
     // updatedText = updatedText.replace(/"\s{2,}"/g, ' ');
     if (text !== updatedText) {
       setDone(true);
-      setToastShown(true);
+      // setToastShown(true);
     } else {
       setDone(false);
-      setToastShown(false);
+      // setToastShown(false);
     }
     return updatedText;
   };
 
-  const processInput = (text) => {
-    let updatedText = text.replace(/“([^”]*)”/g, '«$1»');
-    updatedText = updatedText.replace(/"([^"]*)"/g, '«$1»');
-    updatedText = updatedText.replace(/'([^']*)'/g, '«$1»');
-    while (updatedText.match(/«(.*?)«/g))
-      updatedText = updatedText.replace(/«(.*?)«/g, '«$1"');
-    while (updatedText.match(/»(.*?)»/g))
-      updatedText = updatedText.replace(/»(.*?)»/g, '"$1»');
-
-    // updatedText = updatedText.replace(/"\s{2,}"/g, ' ');
-    if (text !== updatedText) {
-      setDone(true);
-      setToastShown(true);
-    } else {
-      setDone(false);
-      setToastShown(false);
-    }
-    return updatedText;
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(input)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1100);
+      })
+      .catch((err) => console.error('Ошибка копирования:', err));
   };
+
+  // const processInput = (text) => {
+  //   let updatedText = text.replace(/“([^”]*)”/g, '«$1»');
+  //   updatedText = updatedText.replace(/"([^"]*)"/g, '«$1»');
+  //   updatedText = updatedText.replace(/'([^']*)'/g, '«$1»');
+  //   while (updatedText.match(/«(.*?)«/g))
+  //     updatedText = updatedText.replace(/«(.*?)«/g, '«$1"');
+  //   while (updatedText.match(/»(.*?)»/g))
+  //     updatedText = updatedText.replace(/»(.*?)»/g, '"$1»');
+
+  //   // updatedText = updatedText.replace(/"\s{2,}"/g, ' ');
+  //   if (text !== updatedText) {
+  //     setDone(true);
+  //     setToastShown(true);
+  //   } else {
+  //     setDone(false);
+  //     setToastShown(false);
+  //   }
+  //   return updatedText;
+  // };
 
   return (
     <>
@@ -87,6 +101,20 @@ function Input({ done, setDone, setToastShown }) {
           }}
         />
       </div>
+      {done && (
+        <div className="copy-button">
+          <button
+            onClick={handleCopy}
+            className="bg-zinc-700 text-white px-2 py-1.5 rounded-lg border border-zinc-800 cursor-pointer flex shadow-sm"
+          >
+            {copied ? (
+              <span className="icon-[tabler--copy-check]"></span>
+            ) : (
+              <span className="icon-[tabler--copy]"></span>
+            )}
+          </button>
+        </div>
+      )}
     </>
   );
 }
