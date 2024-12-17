c827bfe5ec30de5937b340ab92549e5b7bda518c
siebentod <33163165+siebentod@users.noreply.github.com>
2024-12-17 21:10:32 +0300

    smallfix

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,8 @@
 export const processPdf = (text) => {
   // console.log(text.split(''));
-  for (let i = 0; i < 40; i++) {
-    // console.log(text[i], text.charCodeAt(i).toString(16));
-  }
+  // for (let i = 0; i < 40; i++) {
+  //   // console.log(text[i], text.charCodeAt(i).toString(16));
+  // }
   let updatedText = text.replace(/\u00AD\n/g, ''); // soft hyphen
 
   updatedText = updatedText.replace(/-\n/g, '');
@@ -11,6 +11,7 @@ export const processPdf = (text) => {
   updatedText = updatedText.replace(/ \n/g, ' ');
 
   updatedText = updatedText.replace(/ \n/g, ' ');
+  updatedText = updatedText.replace(/\p{L}\n\p{L}/gu, ' ');
   // updatedText = updatedText.replace(/"([^"]*)"/g, '«$1»');
   // updatedText = updatedText.replace(/'([^']*)'/g, '«$1»');
   // while (updatedText.match(/«(.*?)«/g))
